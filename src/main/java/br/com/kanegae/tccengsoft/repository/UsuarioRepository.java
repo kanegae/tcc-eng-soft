38baf9f99f1c54adab86a8c6864873414da7b92c
Anderson Kanegae <anderson.kanegae@gmail.com>
2021-04-01 17:37:12 -0300

    Corrigindo nome do repositório de usuários.

---
+++ src/main/java/br/com/kanegae/tccengsoft/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.com.kanegae.tccengsoft.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.kanegae.tccengsoft.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
