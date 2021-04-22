11f430ca8ecbc376c2a53e08c51cfff7c509c16e
Anderson Kanegae <anderson.kanegae@gmail.com>
2021-04-21 22:38:42 -0300

    Refatorando modelo de dados para adicionar Papel para Usuário.

--- src/main/java/br/com/kanegae/tccengsoft/model/Usuario.java
+++ src/main/java/br/com/kanegae/tccengsoft/model/Usuario.java
@@ -1,24 +1,35 @@
 package br.com.kanegae.tccengsoft.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	// TODO transformar nome em ID?
 	private String nome;
+	// TODO e-mail deve ser único?
 	private String email;
 	private String senha;
 	@OneToMany(mappedBy = "dono", cascade = CascadeType.ALL)
 	private List<Projeto> projetos;
+	@OneToMany(fetch = FetchType.EAGER)
+	private List<Papel> papeis = new ArrayList<Papel>();
 
 	public Long getCodigo() {
 		return codigo;
@@ -58,5 +69,48 @@ public class Usuario {
 
 	public void setProjetos(List<Projeto> projetos) {
 		this.projetos = projetos;
+	}
+
+	public List<Papel> getPermissoes() {
+		return papeis;
+	}
+
+	public void setPermissoes(List<Papel> papeis) {
+		this.papeis = papeis;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.papeis;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
