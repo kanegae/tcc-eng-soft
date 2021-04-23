0244359937f677e85352b277fc91cc31b27b1ae9
Anderson Kanegae <anderson.kanegae@gmail.com>
2021-04-23 00:45:21 -0300

    Adicionando implementação básica de cadastro de projetos baseado no modelo de cadastro.

---
+++ src/main/java/br/com/kanegae/tccengsoft/controller/ProjetoController.java
@@ -0,0 +1,89 @@
+package br.com.kanegae.tccengsoft.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.kanegae.tccengsoft.model.Projeto;
+import br.com.kanegae.tccengsoft.service.ProjetoService;
+
+@Controller
+@RequestMapping("/projeto")
+public class ProjetoController {
+
+	private ProjetoService service;
+
+	@Autowired
+	public ProjetoController(ProjetoService service) {
+		this.service = service;
+	}
+
+	@GetMapping
+	public ModelAndView listar(Model model) {
+		ModelAndView modelAndView = new ModelAndView("projeto/lista");
+
+		List<Projeto> projetos = service.listar();
+		model.addAttribute("projetos", projetos);
+
+		return modelAndView;
+	}
+
+	@GetMapping("/formulario")
+	public ModelAndView formulario(Model model) {
+		ModelAndView modelAndView = new ModelAndView("projeto/formulario");
+		
+		model.addAttribute("projeto", new Projeto());
+		
+		return modelAndView;
+	}
+
+	@PostMapping
+	public ModelAndView gravar(@ModelAttribute("projeto") Projeto projeto) {
+		service.gravar(projeto);
+
+		// TODO verificar redirect
+		ModelAndView modelAndView = new ModelAndView("redirect:projeto");
+		return modelAndView;
+	}
+	
+	@GetMapping("/{codigo}")
+	public ModelAndView exibir(@PathVariable("codigo") Long codigo, Model model) {
+		ModelAndView modelAndView = new ModelAndView("projeto/formulario");
+		
+		Projeto projeto = service.findById(codigo);
+		
+		model.addAttribute("projeto", projeto);
+		
+		modelAndView.addObject("projeto", projeto);
+		return modelAndView;
+	}
+	
+	// TODO revisar método: requisições DELETE não funcionam via HTML
+	@DeleteMapping("/{codigo}")
+	public ModelAndView excluirOLD(@PathVariable("codigo") Long codigo) {
+		service.excluir(codigo);
+
+		// TODO verificar redirect
+		ModelAndView modelAndView = new ModelAndView("redirect:/projeto");
+		return modelAndView;
+	}
+	
+	// TODO revisar método
+	@PostMapping("/{codigo}/excluir")
+	public ModelAndView excluir(@PathVariable("codigo") Long codigo) {
+		service.excluir(codigo);
+
+		// TODO verificar redirect
+		ModelAndView modelAndView = new ModelAndView("redirect:/projeto");
+		return modelAndView;
+	}
+}
---
+++ src/main/java/br/com/kanegae/tccengsoft/service/ProjetoService.java
@@ -0,0 +1,38 @@
+package br.com.kanegae.tccengsoft.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.kanegae.tccengsoft.model.Projeto;
+import br.com.kanegae.tccengsoft.repository.ProjetoRepository;
+
+@Service
+public class ProjetoService {
+	private ProjetoRepository repository;
+
+	@Autowired
+	public ProjetoService(ProjetoRepository repository) {
+		this.repository = repository;
+	}
+
+	public List<Projeto> listar() {
+		return repository.findAll();
+	}
+
+	public void gravar(Projeto projeto) {
+		repository.save(projeto);
+	}
+	
+	public void excluir(Long codigo) {
+		repository.deleteById(codigo);
+	}
+	
+	// TODO verificar Optional
+	public Projeto findById(Long codigo) {
+		Optional<Projeto> projeto = repository.findById(codigo);
+		return projeto.get();
+	}
+}
